4233dedbcbbaa7a1f10c50c65af4554a849106e0
mttronc <mrtn.mtth@gmx.de>
2017-10-25 18:11:48 +0200

    add option to use Bootstrap media object styling, done via JavaScript

--- tmpl/default.php
+++ tmpl/default.php
@@ -26,10 +26,12 @@ if ($params->get('fluidwidth','0'))
 .heimatwidget
 {
   <?php echo $width; ?>
-  padding: 5px;
-  color: <?php echo $params->get('color_font','#111111'); ?>;
-  background-color: <?php echo $params->get('color_bg','#ffffff'); ?>;
-  border: 1px solid <?php echo $params->get('color_border','#bbbbbb'); ?>;
+  <?php if (!$params->get('css','0')): ?>
+    padding: 5px;
+    color: <?php echo $params->get('color_font','#111111'); ?>;
+    background-color: <?php echo $params->get('color_bg','#ffffff'); ?>;
+    border: 1px solid <?php echo $params->get('color_border','#bbbbbb'); ?>;
+  <?php endif; ?>
 }
 </style>
 <?php foreach($articles as $article) : ?>
@@ -37,3 +39,26 @@ if ($params->get('fluidwidth','0'))
     <?php echo $article->content; ?>
   </div>
 <?php endforeach; ?>
+
+<?php if ($params->get('css','0')): ?>
+  <?php JHtml::_('jquery.framework', false); ?>
+  <?php JHtml::_('bootstrap.loadCss'); ?>
+  <?php /* remove original styling and add Bootstrap classes */ ?>
+  <script>
+    jQuery('.heimatwidget').find('*').removeAttr('style');
+    jQuery('.heimatwidget').each(function() {
+      var a = jQuery(this).find('a.pointer').detach();
+      jQuery(this).find('.mediadbgalleryfloat').remove();
+      jQuery(this).find('div>div').remove();
+      jQuery(this).prepend(a);
+      jQuery(this).addClass('media');
+      jQuery(this).find('a.pointer').addClass('pull-left');
+      jQuery(this).find('a.pointer>img').addClass('media-object');
+      jQuery(this).find('div').first().addClass('media-body');
+      jQuery(this).find('h3').addClass('media-heading');
+      jQuery(jQuery(this).find('div').first().contents().eq(3))
+        .wrap('<p style="line-height: 15px; text-align: justify;"></p>');
+      jQuery(jQuery(this).find('div').last().contents()).wrap('<b></b>');
+    });
+  </script>
+<?php endif; ?>
