87e883e0bdb5166779bef43fad9ffb96f37b0c65
mttronc <mrtn.mtth@gmx.de>
2017-10-26 18:57:22 +0200

    use option width only in original MyHeimat layout

--- tmpl/default.php
+++ tmpl/default.php
@@ -3,7 +3,7 @@
 * @package     Joomla.Site
 * @subpackage  mod_heimatnews
 *
-* @copyright   Copyright (C) 2016 mttronc
+* @copyright   Copyright (C) 2016-2017 mttronc
 * @license     GNU General Public License version 2 or later; see LICENSE.txt
 */
 
@@ -11,26 +11,28 @@ defined('_JEXEC') or die;
 
 if (!$articles)
   echo '<span class="label label-important">Keine Beträge gefunden.</span>';
-
-$width = $params->get('width','450') - 12;
-if ($params->get('fluidwidth','0')):
-  $width = 'max-width: ' . $width . 'px;';
-else:
-  $width = 'width: ' . $width . 'px;';
-endif;
 ?>
-<style>
-.heimatwidget
-{
-  <?php echo $width; ?>
-  <?php if (!$params->get('css','0')): ?>
+
+<?php if (!$params->get('css','0')):
+  $width = $params->get('width','450') - 12;
+  if ($params->get('fluidwidth','0')):
+    $width = 'max-width: ' . $width . 'px;';
+  else:
+    $width = 'width: ' . $width . 'px;';
+  endif;
+?>
+  <style>
+  .heimatwidget
+  {
+    <?php echo $width; ?>
     padding: 5px;
     color: <?php echo $params->get('color_font','#111111'); ?>;
     background-color: <?php echo $params->get('color_bg','#ffffff'); ?>;
     border: 1px solid <?php echo $params->get('color_border','#bbbbbb'); ?>;
-  <?php endif; ?>
-}
-</style>
+  }
+  </style>
+<?php endif; ?>
+
 <?php foreach($articles as $article) : ?>
   <div class="heimatwidget">
     <?php echo $article->content; ?>
