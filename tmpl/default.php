b99c29bc534f5583f6fc8ca673f0fcae2f49933a
mttronc <mrtn.mtth@gmx.de>
2017-11-04 17:35:40 +0100

    add JS to header, run height script in window.onload instead document.ready

--- tmpl/default.php
+++ tmpl/default.php
@@ -11,6 +11,49 @@ defined('_JEXEC') or die;
 
 if (!$articles)
   echo '<span class="label label-important">Keine Beträge gefunden.</span>';
+
+$img_pull = $params->get('img_pull','left');
+
+/* remove original styling and add Bootstrap classes */
+$js = <<<JS
+  jQuery(document).ready(function() {
+    jQuery('.heimatwidget').find('*').removeAttr('style');
+    jQuery('.heimatwidget').each(function() {
+      var a = jQuery(this).find('a.pointer').detach();
+      var h = jQuery(this).find('h3>a').detach();
+      jQuery(this).find('h3').remove();
+      jQuery(this).find('.mediadbgalleryfloat').remove();
+      jQuery(this).find('div>div').remove();
+      jQuery(this).prepend(a);
+      jQuery(this).addClass('media');
+      jQuery(this).find('a.pointer')
+        .addClass('pull-${img_pull}');
+      jQuery(this).find('a.pointer>img').addClass('media-object');
+      jQuery(this).find('div').first().addClass('media-body');
+      jQuery(this).find('div').first().prepend(h);
+      jQuery(jQuery(this).find('div>a').contents()).wrap('<h4></h4>');
+      jQuery(this).find('h4').addClass('media-heading');
+      jQuery(jQuery(this).find('div').first().contents().eq(3))
+        .wrap('<p style="line-height: 15px; text-align: justify;"></p>');
+      jQuery(jQuery(this).find('div').last().contents()).wrap('<b></b>');
+    });
+  });
+JS;
+$js_height = <<<JS
+  jQuery(window).on('load', function() {
+    jQuery('.heimatwidget').each(function() {
+      var height = jQuery(jQuery(this).find('a.pointer>img')).height() + 10;
+      var div = jQuery(this).find('.media-body');
+      var text = jQuery(div).find('p').text().split(' ');
+      jQuery(div).attr('style', 'height: ' + height + 'px;');
+      while (div.prop('scrollHeight') > div.height()) {
+        text.pop();
+        jQuery(div).find('p').html(text.join(' ') + '...');
+      }
+    });
+  });
+JS;
+
 ?>
 
 <?php if (!$params->get('css','0')):
@@ -39,45 +82,13 @@ if (!$articles)
   </div>
 <?php endforeach; ?>
 
-<?php if ($params->get('css','0')): ?>
-  <?php JHtml::_('jquery.framework', false); ?>
-  <?php JHtml::_('bootstrap.loadCss'); ?>
-  <?php /* remove original styling and add Bootstrap classes */ ?>
-  <script>
-    jQuery('.heimatwidget').find('*').removeAttr('style');
-    jQuery('.heimatwidget').each(function() {
-      var a = jQuery(this).find('a.pointer').detach();
-      var h = jQuery(this).find('h3>a').detach();
-      jQuery(this).find('h3').remove();
-      jQuery(this).find('.mediadbgalleryfloat').remove();
-      jQuery(this).find('div>div').remove();
-      jQuery(this).prepend(a);
-      jQuery(this).addClass('media');
-      jQuery(this).find('a.pointer')
-        .addClass('pull-<?php echo $params->get('img_pull','left'); ?>');
-      jQuery(this).find('a.pointer>img').addClass('media-object');
-      jQuery(this).find('div').first().addClass('media-body');
-      jQuery(this).find('div').first().prepend(h);
-      jQuery(jQuery(this).find('div>a').contents()).wrap('<h4></h4>');
-      jQuery(this).find('h4').addClass('media-heading');
-      jQuery(jQuery(this).find('div').first().contents().eq(3))
-        .wrap('<p style="line-height: 15px; text-align: justify;"></p>');
-      jQuery(jQuery(this).find('div').last().contents()).wrap('<b></b>');
-    });
+<?php
 
-    <?php if ($params->get('fixedheight','1')): ?>
-      jQuery(document).ready(function() {
-        jQuery('.heimatwidget').each(function() {
-          var height = jQuery(jQuery(this).find('a.pointer>img')).height() + 10;
-          var div = jQuery(this).find('.media-body');
-          var text = jQuery(div).find('p').text().split(' ');
-          jQuery(div).attr('style', 'height: ' + height + 'px;');
-          while (div.prop('scrollHeight') > div.height()) {
-            text.pop();
-            jQuery(div).find('p').html(text.join(' ') + '...');
-          }
-        });
-      });
-    <?php endif; ?>
-  </script>
-<?php endif; ?>
+if ($params->get('css','0')):
+  JHtml::_('jquery.framework', false);
+  JHtml::_('bootstrap.loadCss');
+  JFactory::getDocument()->addScriptDeclaration($js);
+  if ($params->get('fixedheight','1')):
+    JFactory::getDocument()->addScriptDeclaration($js_height);
+  endif;
+endif;
